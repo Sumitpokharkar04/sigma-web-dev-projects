e1c5a4ea99cfc674158404cae519ccc251940c05
Sumitpokharkar04 <pokharkarsumit04@gmail.com>
2026-01-11 22:00:59 +0530

    object oriented programmig

---
+++ JavaScript Course/More javascript/arrowFunction.js
@@ -0,0 +1,13 @@
+function print() {
+    console.log("This is normal Function");
+}
+
+// const print = () => {
+//     console.log("This is normal Function");
+// }
+
+const printName = (firstName) => {
+    console.log(`Hii , Welcome ${firstName}`);
+}
+
+printName("Sahil");
---
+++ JavaScript Course/More javascript/script.js
@@ -0,0 +1,42 @@
+let a ;
+a= 4;
+console.log(a);
+
+var b;
+b= "Hello World";
+console.log(b);
+
+var undefinedVar;
+console.log(undefinedVar);
+undefinedVar = "Now I am defined!";
+console.log(undefinedVar);
+
+console.log(c); // undefined
+var c =30;
+
+let student1 = {
+    name: "Alice",
+    marks: 85
+}
+
+let student2 = {
+    name: "Bob",
+    marks: 92
+}
+console.log(student1);
+console.log(student2);
+
+let name = "Charlie";
+
+name = "8";
+
+console.log(name);
+
+console.log(typeof name);
+
+let z = 4;
+
+if(a===z)
+{
+    console.log("a and b are equal");
+}
