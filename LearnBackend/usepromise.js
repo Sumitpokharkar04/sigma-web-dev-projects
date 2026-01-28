ae86c0d7af11a79e4a11738dcf8ff374c063b39f
Sumitpokharkar04 <pokharkarsumit04@gmail.com>
2026-01-28 16:50:57 +0530

    Commented out fs.readFile example

--- LearnBackend/usepromise.js
+++ LearnBackend/usepromise.js
@@ -1,8 +1,8 @@
 import fs from "fs"
 
-// let a = await fs.readFile("harry.txt",(error,data)=>{
-//     console.log(data.toString());
-// });
+let a = await fs.readFile("harry.txt",(error,data)=>{
+    console.log(data.toString());
+});
 
 // let b = await fs.writeFile("harry.txt.","i have written something");
 let c = await fs.appendFile("harry.txt","\nkaise ho bhai",(error)=>{
