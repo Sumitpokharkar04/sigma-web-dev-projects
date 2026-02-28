7bfec0bfc82af73fcd3cd20791e9210dc24e37a6
Sumitpokharkar04 <pokharkarsumit04@gmail.com>
2026-02-28 18:01:35 +0530

    commited remaining changes

---
+++ JavaScript Course/JavascriptRevision/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountName = "Kotak";
+var accountCity = "Pune";
+accountHolder = "Sumit";
+
+//const cant be changed
+//var - do not respects scoping {...}
+//We can declare or allocate memory space for data without using any method like let,const or var.
+
+//To print all the variables in one go we can use console.table([]);
+
+console.table([accountId,accountCity,accountName,accountHolder]);
